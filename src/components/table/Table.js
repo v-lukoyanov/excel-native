ecbdcdb5eb24fc425b8d280fd063270ff540ef4a
Vyacheslav Lukoyanov <slaviandzr@gmail.com>
2020-08-01 01:16:10 +0300

    render table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,9 +1,12 @@
 import {ExcelComponent} from '@core/ExcelComponent';
+import {createTable} from '@/components/table/table.template';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
   toHTML() {
+    return createTable();
+    /*
     return `
       <div class="row">
 
@@ -129,5 +132,6 @@ export class Table extends ExcelComponent {
         </div>
       </div>
     `;
+    */
   }
 }
